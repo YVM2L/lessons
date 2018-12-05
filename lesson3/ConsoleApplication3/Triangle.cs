041516d0a3639cf8c68be0c5a08d49e75c987ed3
YVM2L <45400445+YVM2L@users.noreply.github.com>
2018-12-05 17:33:38 +0300

    Update Triangle.cs

--- lesson3/ConsoleApplication3/Triangle.cs
+++ lesson3/ConsoleApplication3/Triangle.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication3
 {
-    class Triangle
+    class Triangle: Figure
     {
+        public Triangle (int x);
+        {
+            this.x = x;
+        }
+        public int t. Square ()
+        {   
+            int t = 1 / 2 * Math.Pow(x,2);
+            return t;
+        }
     }
 }

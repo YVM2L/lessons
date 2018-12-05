ece1b7f03772822334f4db4d736bd863f5922b2a
YVM2L <45400445+YVM2L@users.noreply.github.com>
2018-12-05 17:21:35 +0300

    Update Figure.cs

--- lesson3/ConsoleApplication3/Figure.cs
+++ lesson3/ConsoleApplication3/Figure.cs
@@ -8,9 +8,22 @@ namespace ConsoleApplication3
 {
     class Figure
     {
+        public int x;
+        public int y;
+        public int z;
+        
+        public Figure() {}
+        
+        public Figure (int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        
         public double Square()
         {
-            return -s;
+            return -1;
         }
     }
 }
